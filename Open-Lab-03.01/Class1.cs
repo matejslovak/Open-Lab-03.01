9869f7edefebb4c7fac22f2ff17318038357409e
matejslovak <joggurtik1@gmail.com>
2019-11-04 09:17:44 +0100

    ezz

---
+++ Open-Lab-03.01/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_03._01
+{
+    class Class1
+    {
+    }
+}
---
+++ Open-Lab-03.01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Open_Lab_03._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[4, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 20, 30 } };
+            Console.WriteLine(array[2, 1]);
+        }
+    }
+}
